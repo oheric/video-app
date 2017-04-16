74c27ba6c87839b179419f4610dcb5cc5c0a242e
oheric <ohericyoung@gmail.com>
2017-04-15 23:43:48 -0700

    Initialize Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VideoApp::Application.config.secret_key_base = 'b8ffd5f5f3423ce2ac4f35b440b3c08bd3605fe700244542a44a44467413a61b4ac18c2c8edb73419ad7072ae3bd9847687f4711f515d54fb32227da81a9c977'
