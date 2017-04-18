d85814cd346a535465a4992a87f8f7405e0d9da4
oheric <ohericyoung@gmail.com>
2017-04-18 00:07:43 -0700

    Change iframes to divs that preload images instead of videos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,31 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("DOMContentLoaded",
+    function() {
+        var div, n,
+            v = document.getElementsByClassName("youtube-player");
+        for (n = 0; n < v.length; n++) {
+            div = document.createElement("div");
+            div.setAttribute("data-thumb", v[n].dataset.thumb);
+            div.innerHTML = labnolThumb(v[n].dataset.thumb);
+            div.onclick = labnolIframe;
+            v[n].appendChild(div);
+        }
+    });
+
+function labnolThumb(thumb) {
+    var thumbnail = '<img src="THUMB">',
+        play = '<div class="play"></div>';
+    return thumbnail.replace("THUMB", thumb) + play;
+}
+
+function labnolIframe() {
+    var iframe = document.createElement("iframe");
+    var embed = "https://www.youtube.com/embed/ID?autoplay=1";
+    iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
+    iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("allowfullscreen", "1");
+    this.parentNode.replaceChild(iframe, this);
+}
