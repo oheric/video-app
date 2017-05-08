69eae9cb3bcc87acc6e6f806d93da8c40f3f8cbf
oheric <ohericyoung@gmail.com>
2017-05-07 23:29:02 -0700

    Add overflow:hidden to card

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @reddit_videos = RedditVideo.get("https://www.reddit.com/r/videos/.json")["data"]["children"][0..5]
     @videos = Video.all
   end
 end
