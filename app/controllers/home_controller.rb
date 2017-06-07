2de32c63390575469febb1c5f5d97d04b30ddd2f
oheric <ohericyoung@gmail.com>
2017-06-07 00:12:08 -0700

    Get actual URL of youtube videos, not the embed version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     @reddit_videos = RedditVideo.get("https://www.reddit.com/r/videos/.json")["data"]["children"][0..5]
-    @videos = Video.all
+    @videos = [OEmbed::Providers.get("https://youtu.be/4VnMmBmiKw8")]
+    # Video.all.each do |v|
+    #   @url = v.youtube_url.split(" ")[3].partition('=').last.rchomp('"').chomp('"') + "&autoplay=1"
+    #   @videos << OEmbed::Providers.get(@url)
+    # end
   end
 end
